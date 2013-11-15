e726694fcd45a9a3ddcff89b65b1c816548ff396
James R. Qualls <Qualls.James@gmail.com>
2013-11-15 17:24:40 -0500

    ready

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
   // Deal with OPTIONS method
-  if (req.method is "OPTIONS") {
+  if (req.method === "OPTIONS") {
     res.send(200)
   } else {
     next();
